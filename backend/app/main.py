dcbaffc84ce75f980e9eaf8f09dbff0e1e91fc18
Your Name <your_email@example.com>
2026-06-26 15:51:54 +0530

    Fix Render CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -73,7 +73,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://mailpilot-ai-3.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
